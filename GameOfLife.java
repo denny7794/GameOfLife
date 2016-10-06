1f77ae52e115942df713aa36a249aa20bb94ac78
denny7794 <denny7794@gmail.com>
2016-10-06 12:21:53 +0400

    заголовок окна

--- GameOfLife.java
+++ GameOfLife.java
@@ -7,12 +7,17 @@ import java.awt.*;
 import javax.swing.*;
 
 public class GameOfLife {
+
+	final String NAME_OF_GAME = "Conway's Game of Life";
+	JFrame frame;
+	Canvas canvasPanel;
+
 	public static void main(String[] args) {
 		new GameOfLife().go();
 	}
 	
 	void go() {
-		JFrame frame = new JFrame();
+		frame = new JFrame(NAME_OF_GAME);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(300, 300);
 		frame.setLocation(200, 200);
