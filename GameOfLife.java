d85e7bf0763fc72152836080c80a3be4d550e4a6
denny7794 <denny7794@gmail.com>
2016-10-06 13:03:33 +0400

    Add files via upload

---
+++ GameOfLife.java
@@ -0,0 +1,17 @@
+/**
+* Conway's Game of Life
+*
+*/
+
+import java.awt.*;
+import javax.swing.*;
+
+public class GameOfLife {
+	public static void main(String[] args) {
+		new GameOfLife().go();
+	}
+	
+	void go() {
+		System.out.println("Conway's Game of Life");
+	}
+}
